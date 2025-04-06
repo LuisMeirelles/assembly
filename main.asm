f88328297247805d2e69c3b7d7bbcd99bc5ee84c
Luis Meirelles <luis.gustavo@essentia.com.br>
2025-04-05 23:49:36 -0300

    feat: add println procedure
    println is a recursive procedure that no more depends on a provided
    string length, detecting the end of the string with the presence of 0x0
    byte

--- main.asm
+++ main.asm
@@ -1,24 +1,33 @@
 section .data
-  string db "teste", 0xa, 0
-  string_len db 6
+  string db "testando", 0xa, 0
 
 section .text
   global _start
 
 println:
-  MOV rax, 1
-  MOV rdi, 1
-  ; we can use rsi as parameter here, so code below is not needed
+  MOVZX eax, byte [rsi] ; moves zero-extended version of the byte that %rsi is pointing to
+  CMP al, 0
+  JE return
+
+  MOV rax, 1 ; write syscall
+  MOV rdi, 1 ; stdout
+  
+  ; uses rsi as parameter here, so code below is not needed
   ; MOV rsi, string
-  MOV rdx, [string_len]
+  
+  MOV rdx, 1 ; chars count to be printed
   SYSCALL
-  RET
+
+  INC rsi
+  JMP println
+
+  return: RET
 
 _start:
-  MOV rsi, string
+  LEA rsi, [rel string] ; loading relative address of string head to %rsi
   CALL println
 
-  MOV rax, 60
-  MOV rdi, 0
+  MOV rax, 60 ; exit syscall
+  MOV rdi, 0 ; exit code
   SYSCALL
 
